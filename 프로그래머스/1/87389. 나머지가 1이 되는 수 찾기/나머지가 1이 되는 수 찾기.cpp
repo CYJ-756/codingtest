973c4652f1bbaf6a8751dab424f64678ba8e041a
CYJ-756 <cdj91102363@gmail.com>
2025-05-26 14:08:57 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 2.23 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int i = 1;
+    while(1){
+        if(n % i == 1){
+            answer = i;
+            break;
+        }
+        i++;
+    }
+    return answer;
+}
