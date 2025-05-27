885bcf6c16a97500e28d456d5df18dafb35f156d
CYJ-756 <cdj91102363@gmail.com>
2025-05-27 11:37:19 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
-
     for (int i = 0; i < 10; i++){
         answer += n % 10;
         n /= 10;
