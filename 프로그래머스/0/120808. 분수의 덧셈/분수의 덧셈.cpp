1d14b345fa05691877ca75e1913c0f6f8a73f1a2
CYJ-756 <cdj91102363@gmail.com>
2025-05-27 11:01:55 +0900

    Update 분수의 덧셈.cpp

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -7,7 +7,8 @@ vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
     vector<int> answer;
     
     int x = numer1 * denom2 + denom1 * numer2;
-    int y = denom1 * denom2;int a = x, b = y;
+    int y = denom1 * denom2;
+    int a = x, b = y;
     
     while (b != 0) {
         int temp = a % b;
