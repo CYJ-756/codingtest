7a1472275dc493f6234c221aa7ae7a0017feb236
CYJ-756 <cdj91102363@gmail.com>
2025-05-27 16:38:10 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    if (s.length() != 4 && s.length() != 6)
+        return false;
+    bool answer = true;
+    for (int i = 0; i < s.length(); i++){
+         if (!isdigit(s[i]))
+            return false;
+    }
+    return true;
+}
