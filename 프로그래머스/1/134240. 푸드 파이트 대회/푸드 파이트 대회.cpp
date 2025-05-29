f06fac7129159dd91da2985d8270e566a84596db
CYJ-756 <cdj91102363@gmail.com>
2025-05-29 14:31:26 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.55 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    for (int i = 1; i < food.size(); i++){
+        for(int j = 0; j < food[i] / 2; j++){
+            answer += to_string(i);
+        }
+    }
+    string k = answer;
+    reverse(k.begin(),k.end());
+    return answer + "0" + k;
+}
