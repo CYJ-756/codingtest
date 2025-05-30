aea56518239ef1751e6b567dd74ca44381523a18
CYJ-756 <cdj91102363@gmail.com>
2025-05-30 10:22:46 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -9,11 +9,9 @@ string solution(vector<string> cards1, vector<string> cards2, vector<string> goa
     int cards1_stack = 0;
     int cards2_stack = 0;
     for(int i = 0; i < goal.size(); i++){
-        auto it_1 = find(cards1.begin(), cards1.end(), goal[i]);
-        auto it_2 = find(cards2.begin(), cards2.end(), goal[i]);
-        if(cards1_stack < cards1.size() && cards1[cards1_stack] == *it_1){
+        if(cards1_stack < cards1.size() && cards1[cards1_stack] == goal[i]){
             cards1_stack++;
-        }else if(cards2_stack < cards2.size() && cards2[cards2_stack] == *it_2){
+        }else if(cards2_stack < cards2.size() && cards2[cards2_stack] == goal[i]){
             cards2_stack++;
         }else{
             answer = "No";
